ebb7f81f8ba0e1842bc2e37954723a975c903acf
Matias P. <matias.penailillob@gmail.com>
2024-09-23 16:03:13 -0300

    feat: migrations

---
+++ prisma/migrations/20240923185830_add_relationships/migration.sql
@@ -0,0 +1,17 @@
+-- CreateTable
+CREATE TABLE "_NoteTags" (
+    "A" INTEGER NOT NULL,
+    "B" INTEGER NOT NULL
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "_NoteTags_AB_unique" ON "_NoteTags"("A", "B");
+
+-- CreateIndex
+CREATE INDEX "_NoteTags_B_index" ON "_NoteTags"("B");
+
+-- AddForeignKey
+ALTER TABLE "_NoteTags" ADD CONSTRAINT "_NoteTags_A_fkey" FOREIGN KEY ("A") REFERENCES "Note"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_NoteTags" ADD CONSTRAINT "_NoteTags_B_fkey" FOREIGN KEY ("B") REFERENCES "Tag"("id") ON DELETE CASCADE ON UPDATE CASCADE;
