d18dc4a1df84b768ce409d70e0c18aabee6173b6
Matias P. <matias.penailillob@gmail.com>
2024-09-24 12:00:46 -0300

    feat(create-user.dto): add dto validations

--- src/users/infrastructure/dto/create-user.dto.ts
+++ src/users/infrastructure/dto/create-user.dto.ts
@@ -1,5 +1,17 @@
+import { IsEmail, IsNotEmpty, IsString, IsStrongPassword } from "class-validator";
+
 export class CreateUserDto {
+    
+    @IsNotEmpty()
+    @IsString()
     username: string;
+
+    @IsNotEmpty()
+    @IsEmail()
+    @IsString()
     email: string;
+
+    @IsStrongPassword( { minLength: 16 })
+    @IsNotEmpty()
     password: string;
 }
