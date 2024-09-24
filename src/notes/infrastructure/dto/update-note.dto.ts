1fbbf60c134cde0d8375241860d7553a44318e2b
Matias P. <matias.penailillob@gmail.com>
2024-09-24 11:52:56 -0300

    feat(update-note.dto): add dto validations

--- src/notes/infrastructure/dto/update-note.dto.ts
+++ src/notes/infrastructure/dto/update-note.dto.ts
@@ -1,5 +1,18 @@
+import { IsArray, IsNotEmpty, IsNumber, IsOptional, IsString } from "class-validator";
+
 export class UpdateNoteDto {
+
+    @IsNotEmpty({ message: 'El título es obligatorio' })
+    @IsString({ message: 'El título debe ser un string' })
     title: string;
+
+    @IsNotEmpty({ message: 'El contenido es obligatorio' })
+    @IsString({ message: 'El contenido debe ser un string' })
     content: string;
+
+
+    @IsOptional()
+    @IsArray({ message: 'Las etiquetas deben ser un array' })
+    @IsNumber({}, { each: true, message: 'Cada etiqueta  debe ser un número' })
     tags?: number[];
 }
