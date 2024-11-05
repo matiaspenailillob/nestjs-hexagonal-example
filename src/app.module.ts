fcacd2060f8af13470cd723276ac00052cfdd3f4
Matias P. <matias.penailillob@gmail.com>
2024-11-05 15:53:51 -0300

    feat: manage env variables - Install dot env and nest config - Configure app.module to make env variables are available globally - modify auth.module to use ConfigService and get env variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,17 @@ import { NotesModule } from './notes/notes.module';
 import { UsersModule } from './users/users.module';
 import { PrismaService } from './prisma/prisma.service';
 import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [NotesModule, UsersModule, AuthModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true // Hace que ConfigModule este disponible en toda la aplicación.
+    }),
+    NotesModule,
+    UsersModule,
+    AuthModule
+  ],
   controllers: [AppController],
   providers: [PrismaService, AppService],
   exports: [PrismaService]
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
 import { HashService } from './infrastructure/services/hash.service';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
+    imports: [
+        PassportModule,
+        JwtModule.registerAsync({
+            imports: [ConfigModule],
+            inject: [ConfigService], // permite acceder a las variables de entorno.
+            useFactory: async (configService: ConfigService) => ({
+                secret: configService.get<string>('JWT_SECRET'),
+                signOptions: { expiresIn: configService.get<string>('JWT_EXPIRATION')}
+            })
+        })
+    ],
     providers: [
-        HashService
+        HashService,
     ],
     exports: [ 
         HashService
